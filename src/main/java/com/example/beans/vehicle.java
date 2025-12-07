0e4ed99fe108ff58170fbac8fde119c31f400e5b
Lenovo <Saurabh2002jpl@gmail.com>
2025-12-07 22:01:10 +0530

    SpringBootProject

---
+++ src/main/java/com/example/beans/vehicle.java
@@ -0,0 +1,13 @@
+package com.example.beans;
+
+public class vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
