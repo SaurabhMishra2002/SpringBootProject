0fa9021db7605fe79385f388f499f025d39099d3
Lenovo <Saurabh2002jpl@gmail.com>
2025-12-08 23:57:57 +0530

    Example6 added

--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -1,23 +1,5 @@
 package com.example.config;
 
-import com.example.beans.Vehicle;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.beans")
 public class ProjectConfig {
-
-    private String name;
-
-    @Bean(initMethod = "initialize")
-    public Vehicle vehicle() {
-        return new Vehicle();
-    }
-
-    public void initialize() {
-        this.name = "Honda";
-    }
 
 }
